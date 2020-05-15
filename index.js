dcce1af22c0979be800ff1d1abfbbb7069a246bf
MaryamMosstoufi <maryam@zooxoos.com>
2020-05-15 13:53:44 -0400

    JS Stretch 2

--- index.js
+++ index.js
@@ -330,11 +330,26 @@ The function should console.log 50 chunks of HTML code that match the structure 
 
 ‼️ You do **NOT** need to get these to display on your page, but you can copy and paste the result into your HTML file if you'd like to see what that would look like. */
 
-function getHTML(/* Code here */) {
-
-  /* Code here */
-
+function getHTML(artists) {
+  let allFiftyHtml = "";
+  for (let i = 0; i < artists.length; i++) {
+    let
+      allFiftyHtml +=
+    "<div class=\"artist\"> <div class=\"image\"> <img src=\"assets/" +
+      artists[i].name.replace(/\s+/g, '-') +
+      ".jpg \"/> </div> <div class=\"name\"> <a href=\"" +
+      artists[i].wikipedia +
+      "\">" +
+      artists[i].name +
+      "</a> </div> <div class =\"bio\">" +
+      artists[i].bio +
+      "</div> </div>"
+      ;
+  }
+  return allFiftyHtml;
 }
+
+console.log(getHTML(artists));
 
 
 /* STRETCH 3: Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
@@ -347,3 +362,4 @@ function randomize(/* Code here */) {
 
 
 /* STRETCH 3: Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
+// Note: I did it already.
